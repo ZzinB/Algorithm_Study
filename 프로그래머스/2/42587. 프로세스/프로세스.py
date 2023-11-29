74f35cdc8885dee8db3b9da0315c47f736b17365
이신비 <54674562+ZzinB@users.noreply.github.com>
2023-11-29 22:51:17 +0900

    [level 2] Title: 프로세스, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    queue = deque([(i, p) for i, p in enumerate(priorities)])
+
+    while True:
+        current = queue.popleft()
+
+        if any(current[1] < q[1] for q in queue):
+            queue.append(current)
+        else:
+            answer += 1
+            if current[0] == location:
+                break
+
+    return answer
