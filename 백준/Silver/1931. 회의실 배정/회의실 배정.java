7928ddf464e45e9f95b70f903d131e181d05077f
Shinbi Lee <54674562+ZzinB@users.noreply.github.com>
2024-07-11 21:19:28 +0900

    [Silver I] Title: 회의실 배정, Time: 468 ms, Memory: 44660 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[][] meetings = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            meetings[i][0] = Integer.parseInt(st.nextToken());
+            meetings[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        // 회의를 끝나는 시간 기준으로 정렬, 끝나는 시간이 같으면 시작 시간 기준으로 정렬
+        Arrays.sort(meetings, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                if (a[1] == b[1]) {
+                    return Integer.compare(a[0], b[0]);
+                }
+                return Integer.compare(a[1], b[1]);
+            }
+        });
+
+        int count = 0;
+        int last = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (meetings[i][0] >= last) {
+                last = meetings[i][1];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
